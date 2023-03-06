ee2b89876fe69fcfc88666fc5598c0f5bf4df150
Vallejo Bryan <bryan.vallejo@aalto.fi>
2023-03-06 11:14:56 +0200

    untracked

--- source/conf.py
+++ source/conf.py
@@ -58,7 +58,7 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 html_theme = 'sphinx_book_theme'
-html_last_updated_fmt = None
+html_last_updated_fmt = ''
 # Show todos
 todo_include_todos = True
 
